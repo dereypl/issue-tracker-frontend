b10a6bf949c2dd96ead9de2002dfc3b3bbdc7e00
dereypl <mpasich.pk@gmail.com>
2022-05-15 23:22:22 +0200

    Method updateItem added to httpService.

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -6,3 +6,10 @@ export const fetchItems = ({restUrl, ...params}) => axios
     .get(`${API_URL}/${restUrl}`, {
         params: params,
     })
+
+export const updateItem = ({restUrl, id, data, ...params}) => axios
+    .patch(`${API_URL}/${restUrl}/${id}`, {
+        ...data
+    }, {
+        params: params,
+    })
